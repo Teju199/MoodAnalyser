ee7733eb2bb2b66908224e7c9fe9b4367b3daeee
Teju199 <tejuiyer95@gmail.com>
2022-03-09 23:16:40 +0530

    Exception class added

---
+++ MoodAnalyser/MoodAnalyserException.java
@@ -0,0 +1,9 @@
+package MoodAnalyser;
+//class created for custom exception and it extends Exception class.
+public class MoodAnalyserException extends Exception {
+
+    public MoodAnalyserException(String message) {
+        super(message);
+    }
+}
+
